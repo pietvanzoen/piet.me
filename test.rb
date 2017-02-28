b42806c6b8026e6d9b5d06d0cd10d178e3050dc5
pietvanzoen <pietvanzoen@gmail.com>
2017-02-28 08:40:25 +0100

    enable test debug logging

--- test.rb
+++ test.rb
@@ -4,6 +4,7 @@ require 'html-proofer'
 raise IOError, 'Directory ./build does not exist. Run `middleman build` before running tests' unless Dir.exists?('./build')
 
 HTMLProofer.check_directory('./build', {
+  :log_level => :debug,
   :check_img_http => true,
   :check_html => true, :validation => { :report_missing_names => true },
   :check_favicon => true,
