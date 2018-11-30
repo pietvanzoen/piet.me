00f7d1a730ded23353a2da11cd3fcf3dfad5cff3
pietvanzoen <hi@pietvanzoen.com>
2018-11-30 10:46:13 +0100

    fix: add 301 and 302 to ignore list

--- test.rb
+++ test.rb
@@ -1,14 +1,14 @@
 
-require 'html-proofer'
+require "html-proofer"
 
-raise IOError, 'Directory ./build does not exist. Run `middleman build` before running tests' unless Dir.exists?('./build')
+raise IOError, "Directory ./build does not exist. Run `middleman build` before running tests" unless Dir.exists?("./build")
 
-HTMLProofer.check_directory('./build', {
+HTMLProofer.check_directory("./build", {
   :log_level => :debug,
   :check_img_http => true,
-  :check_html => true, :validation => { :report_missing_names => true },
+  :check_html => true, :validation => {:report_missing_names => true},
   :check_favicon => true,
   :check_opengraph => true,
   :url_ignore => [/pietvanzoen.com/],
-  :http_status_ignore => [0,999,403,401]
+  :http_status_ignore => [0, 999, 403, 401, 301, 302],
 }).run
