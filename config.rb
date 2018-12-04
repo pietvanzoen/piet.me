06d48c1a041ddc58b5049963b6bf477650c7a99a
pietvanzoen <hi@pietvanzoen.com>
2018-12-04 15:47:35 +0100

    add slugs to projects page headers

--- config.rb
+++ config.rb
@@ -59,4 +59,27 @@ helpers do
 
     link_to name, url, options
   end
+
+  def to_slug(str)
+    #strip the string
+    ret = str.strip
+
+    #blow away apostrophes
+    ret.gsub! /['`]/, ""
+
+    # @ --> at, and & --> and
+    ret.gsub! /\s*@\s*/, " at "
+    ret.gsub! /\s*&\s*/, " and "
+
+    #replace all non alphanumeric, underscore or periods with underscore
+    ret.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, "-"
+
+    #convert double underscores to single
+    ret.gsub! /_+/, "-"
+
+    #strip off leading/trailing underscore
+    ret.gsub! /\A[_\.]+|[_\.]+\z/, ""
+
+    ret.downcase
+  end
 end
