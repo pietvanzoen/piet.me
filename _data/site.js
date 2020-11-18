7a261e77f0664ba6ffb7b179af9790249fdab359
pietvanzoen <hi@pietvanzoen.com>
2020-11-18 20:28:12 +0100

    menu uses active state

--- _data/site.js
+++ _data/site.js
@@ -2,10 +2,11 @@ module.exports = {
   buildTime: new Date(),
   url: 'https://piet.me',
   title: 'Piet van Zoen',
+  isProduction: process.env.ELEVENTY_PRODUCTION === 'true',
   menu: [
-    { href: '/updates', name: 'Updates' },
-    { href: '/blog', name: 'Posts' },
-    { href: '/projects', name: 'Projects' },
-    { href: '/about', name: 'About' },
+    { href: '/updates/', name: 'Updates' },
+    { href: '/blog/', name: 'Posts' },
+    { href: '/projects/', name: 'Projects' },
+    { href: '/about/', name: 'About' },
   ],
 };
