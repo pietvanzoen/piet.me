95d8924c3a2f90f2bfc06280122823aa07b48708
pietvanzoen <hi@pietvanzoen.com>
2020-11-19 19:12:41 +0100

    add fathom script

--- _data/site.js
+++ _data/site.js
@@ -1,6 +1,8 @@
+const HOSTNAME = 'piet.me';
 module.exports = {
   buildTime: new Date(),
-  url: 'https://piet.me',
+  url: `https://${HOSTNAME}`,
+  hostname: HOSTNAME,
   title: 'Piet van Zoen',
   isProduction: process.env.ELEVENTY_PRODUCTION === 'true',
   menu: [
