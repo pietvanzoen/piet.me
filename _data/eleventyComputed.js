7cce991102c57c143a1120f5a70d2af422465550
pietvanzoen <hi@pietvanzoen.com>
2020-12-08 09:30:29 +0100

    fix update title

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,8 +1,14 @@
+const dayjs = require('../_lib/dayjs');
 module.exports = {
   absoluteUrl: ({ page, site }) => new URL(page.url, site.url).href,
   seo: {
     siteTitle: ({ site }) => site.title,
-    title: ({ title, site }) => [title, site.title].filter(Boolean).join(' - '),
+    title: (data) => {
+      if (isUpdate(data)) {
+        data.title = dayjs(data.date).format('Do MMM YYYY HH:mm');
+      }
+      return [data.title, data.site.title].filter(Boolean).join(' - ');
+    },
     description: ({ excerpt, description, site }) => description || excerpt || site.description || '',
     type: ({ meta }) => meta?.type || 'website',
     image: ({ meta, unsplashImageId }) => {
@@ -17,3 +23,7 @@ module.exports = {
     },
   },
 };
+
+function isUpdate(data) {
+  return data.page.inputPath.includes('updates/');
+}
